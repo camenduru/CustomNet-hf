e8e1c2493ccb6063d013bd0b9c4ba2838b9d0db8
yzy <jiangyzy@users.noreply.huggingface.co>
2024-04-06 12:29:23 +0000

    Update utils/gradio_utils.py

--- utils/gradio_utils.py
+++ utils/gradio_utils.py
@@ -1,9 +1,20 @@
-from ldm.util import create_carvekit_interface, load_and_preprocess
+from ldm.util import load_and_preprocess
+from carvekit.api.high import HiInterface
 import spaces
 
 
 def load_preprocess_model():
-    carvekit = create_carvekit_interface()
+    carvekit = HiInterface(object_type="object",  # Can be "object" or "hairs-like".
+                        batch_size_seg=5,
+                        batch_size_matting=1,
+                        # device='cuda' if torch.cuda.is_available() else 'cpu',
+                        device='cuda',
+                        seg_mask_size=640,  # Use 640 for Tracer B7 and 320 for U2Net
+                        matting_mask_size=2048,
+                        trimap_prob_threshold=231,
+                        trimap_dilation=30,
+                        trimap_erosion_iters=5,
+                        fp16=False)
     return carvekit
     
 @spaces.GPU
